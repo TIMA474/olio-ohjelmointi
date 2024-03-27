75f5b92e9b540562e2d6f7d05723d00571d185d4
Timur <galkintimur@gmail.com>
2024-03-27 10:42:08 +0200

    Добавьте файлы проекта.

---
+++ harjoitus 3 (KT)/kiuas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_3__KT_
+{
+    internal class kiuas
+    {
+      
+    
+    
+    
+    }
+}
