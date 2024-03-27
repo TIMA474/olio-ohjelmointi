27d176b7a0f4476f8d35e2d632beafcb5230e675
Timur <galkintimur@gmail.com>
2024-03-27 14:07:44 +0200

    kiuas 3 tehty

--- harjoitus 3 (KT)/Program.cs
+++ harjoitus 3 (KT)/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using harjoitus_3__KT_;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Luodaan Kiuas-olio
+        kiuas kiuas = new kiuas();
+
+
+        kiuas.kytkepäälle();
+        kiuas.asetaLämpötila(80);
+        kiuas.asetaKosteus(70);
+        kiuas.kytkepoispäältä();
+
+
+    }
+}
--- harjoitus 3 (KT)/kiuas.cs
+++ harjoitus 3 (KT)/kiuas.cs
@@ -8,9 +8,48 @@ namespace harjoitus_3__KT_
 {
     internal class kiuas
     {
-      
-    
-    
-    
+        private int onkopäällä;
+        private int lämpötila;
+        private int kostetus;
+
+        public kiuas()
+        {
+            // 0 = pois päältä, 1 = päällä
+            onkopäällä = 0;
+
+            // Oletuslämpötila
+            lämpötila = 20;
+
+            // Oletuskosteus
+            kostetus = 50;
+
+        }
+
+        public void kytkepäälle()
+        {
+            onkopäällä = 1;
+            Console.WriteLine(" kiuas on nytten päällä ");
+        }
+
+        public void kytkepoispäältä()
+        {
+            onkopäällä = 0;
+            Console.WriteLine("kiuas on nytten päällä ");
+
+        }
+        public void asetaLämpötila(int lämpö)
+        {
+            lämpötila = lämpö;
+            Console.WriteLine("Lämpötila on asetettu: " + lämpötila + " astetta.");
+        }
+
+        public void asetaKosteus(int kosteudet)
+        {
+            kostetus = kosteudet;
+            Console.WriteLine("Kosteus on asetettu: " + kostetus + "%.");
+
+
+
+        }
     }
 }
